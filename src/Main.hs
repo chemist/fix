a0ff672fa6e3a36c260061145a227eb5d08a0f67
Alexey Smirnov <chemistmail@gmail.com>
2015-03-03 18:37:49 +0300

    opts

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import System.Directory
+-- import System.Directory
 import System.FilePath
+import Options.Applicative
+import Data.Map
 
 
 path :: FilePath
@@ -13,4 +15,79 @@ realp = path </> ".fix"
 
 main :: IO ()
 main = do
-    createDirectoryIfMissing True realp
+    p <- execParser (info opts idm)
+    print p
+--     createDirectoryIfMissing True realp
+
+opts :: Parser Command
+opts = subparser
+  (  command "add" (info (Command Add <$> addOptions)
+      ( progDesc "add" ))
+  <> command "switch" (info (Command Switch <$> switchOptions)
+      ( progDesc "switch" ))
+  )
+
+addOptions :: Parser Entity
+addOptions = subparser
+  (  command "host"    (info (Host <$> sm "HOSTNAME")
+      ( progDesc "host"))
+  <> command "user"    (info (User <$> sm "USER NAME")
+      ( progDesc "user"))
+  <> command "service" (info (Service <$> sm "SERVICE")
+      ( progDesc "service"))
+  <> command "check"   (info (Check <$> sm "PATH")
+      ( progDesc "check"))
+  <> command "template" (info (Template <$> sm "TEMPLATE")
+      ( progDesc "template"))
+  )
+  where
+    sm = strArgument . metavar
+
+switchOptions :: Parser Entity
+switchOptions = subparser
+  (  command "host"    (info (Host <$> sm "HOSTNAME")
+      ( progDesc "host"))
+  <> command "user"    (info (User <$> sm "USER NAME")
+      ( progDesc "user"))
+  <> command "service" (info (Service <$> sm "SERVICE")
+      ( progDesc "service"))
+  <> command "check"   (info (Check <$> sm "PATH")
+      ( progDesc "check"))
+  <> command "template" (info (Template <$> sm "TEMPLATE")
+      ( progDesc "template"))
+  )
+  where
+    sm = strArgument . metavar
+
+data Verbosity = Normal | Verbose deriving (Show, Eq)
+
+verbosity :: Parser Verbosity
+verbosity = flag Normal Verbose
+  ( long "verbose"
+  <> short 'v'
+  <> help "Enable verbose mode" )
+
+data Options = Options
+  { optCommand :: Command
+  , optVerbosity :: Verbosity
+  } deriving (Show, Eq)
+
+data Command = Command Do Entity
+  deriving (Show, Eq)
+
+data Do = Add | Switch | Delete | View deriving (Show, Eq)
+
+data Entity
+  = Host Hostname
+  | Layer Layername [(Weight, Entity, Env)]
+  | User String
+  | Service String
+  | Check Path
+  | Template Path
+  deriving (Show, Eq)
+
+type Path = String
+type Hostname = String
+type Layername = String
+type Weight = Int
+type Env = Map String String

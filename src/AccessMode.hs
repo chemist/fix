5d3e4e3da307d76b36d6a0a0864c705e4ec88c66
Alexey Smirnov <chemistmail@gmail.com>
2015-05-15 16:12:15 +0300

    access mode

--- src/AccessMode.hs
+++ src/AccessMode.hs
@@ -7,6 +7,9 @@ import Data.Map (Map, (!))
 import qualified Data.Map as M
 import Data.Text (Text)
 import Data.Monoid ((<>))
+import Data.Bits
+import System.Posix.Types
+import Text.Printf
 import Prelude hiding (takeWhile)
 
 type PathRegexp = String
@@ -17,9 +20,9 @@ data Owner = NOwner Text
 data Group = NGroup Text
            | IGroup Integer
 
-type FileMode = String
+type FSMode = String
 
-newtype AccessMode = AccessMode (Map PathRegexp (Owner, Group, FileMode))
+newtype AccessMode = AccessMode (Map PathRegexp (Owner, Group, FSMode))
 
 main :: IO ()
 main = putStrLn "hello"
@@ -27,7 +30,7 @@ main = putStrLn "hello"
 comment :: Parser ()
 comment = skipSpace *> char '#' *> skipWhile isEndOfLine *> pure ()
 
-mode :: Parser (PathRegexp, (Owner, Group, FileMode))
+mode :: Parser (PathRegexp, (Owner, Group, FSMode))
 mode = undefined
 
 userid :: Parser Owner
@@ -36,10 +39,10 @@ userid = undefined
 groupid :: Parser Group
 groupid = undefined
 
-fileMode :: Parser FileMode
-fileMode = numMode <|> symMode
+sfileMode :: Parser FSMode
+sfileMode = numMode <|> symMode
 
-numMode :: Parser FileMode
+numMode :: Parser FSMode
 numMode =  count 4 allowed <|> (("0" <>) `fmap` count 3 allowed)
     where
       allowed = satisfy (inClass "01234567")
@@ -51,14 +54,16 @@ goodNumMode = "444"
 goodTextMode :: Text 
 goodTextMode = "rwxrwx---"
 
-symMode :: Parser FileMode
+symMode :: Parser FSMode
 symMode = count 9 allowed
   where
     allowed = satisfy (inClass "rwxstST-")
 
 toTextMode :: String -> String
 toTextMode "" = error "empty num mode"
-toTextMode (s:xs) = sticky $ concatMap toMode xs
+toTextMode (s:xs)
+  | length xs == 2 = concatMap toMode (s:xs)
+  | otherwise = sticky $ concatMap toMode xs
   where
     toMode :: Char -> String
     toMode '0' = "---" 
@@ -114,6 +119,41 @@ toNumMode xs =
       fun ys = foldl (\y x -> pluss y (toMode x)) (0,0) ys
       pluss :: (Int, Int) -> (Int, Int) -> (Int, Int)
       pluss (a,b) (c,d) = (a + c, b + d)
+
+textModeToCMode :: String -> CMode
+textModeToCMode xs =
+    let z = complementBit 0 15
+        withPos = zip [0..] $ reverse xs
+        fun b (_, '-') = b 
+        fun b (i, 't') = complementBit (complementBit b i) 9
+        fun b (_, 'T') = complementBit b 9
+        fun b (i, 's') 
+          | i == 3 = complementBit (complementBit b i) 10
+          | i == 6 = complementBit (complementBit b i) 11
+          | otherwise = error "bad textmode"
+        fun b (i, 'S') 
+          | i == 3 = complementBit b 10
+          | i == 6 = complementBit b 11
+          | otherwise = error "bad textmode"
+        fun b (i, _) = complementBit b i
+    in CMode $ foldl fun z withPos
+
+numModeToCMode :: [Int] -> CMode
+numModeToCMode xs =
+    let z = complementBit 0 15
+        withPos = zip [1 .. ] $ reverse xs
+        fun b (_, 0) = b 
+        fun b (i, n) =
+            let (nr, r) = if (n - 4) >= 0 then (n - 4, complementBit b (3 * i - 1)) else (n, b)
+                (nw, w) = if (nr - 2) >= 0 then (nr - 2, complementBit r (3 * i - 2)) else (nr, r)
+            in if nw == 1  then complementBit w (3 * i - 3) else w
+    in CMode $ foldl fun z withPos
+
+
+numModeToCMode _ = error "bad num mode"
+
+printCMode :: CMode -> String
+printCMode (CMode x) = printf "%b" x
 
 goodComment :: Text
 goodComment = "  # asdfasdfasdf asdfasdf afsd\n\n"
